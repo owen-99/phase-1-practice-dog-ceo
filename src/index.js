a6e73c07daa9de410d78267e2bcd704b724ab79b
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-20 02:35:08 +0300

    commit tree a157b2dcf317239466dacb3b798c085796bbef5c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log('%c HI', 'color: firebrick')
+document. addEventListener('DOMContentloaded',funtion());{
+    fetchingDogImages()
+
+}
+
+function fetchDOgImages(){
+    //alert("")
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+         .then(response=> response.json())
+         .then(data=>{
+            let images =data.message
+            images.forEach(image =>{
+             console.log(image)
+             loadImagestoDOM()
+            });
+        })
+
+
+
+}
+
+function loadImagestoDOM(image){
+    let dogCont = document.getElementById('dog-image-')
+    let ourimage = documnt.createElement('img')
+    ourImage_src = 'image'
+    dogCont.appendChild (ourimage)
+}
